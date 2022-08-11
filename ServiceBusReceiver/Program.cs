e4fdebfdf225d2f57040cbc7c4060a22cfbc4108
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-08-11 07:58:47 +0200

    Implement receiver

--- ServiceBusReceiver/Program.cs
+++ ServiceBusReceiver/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using ServiceBusShared.Models;
 
 namespace ServiceBusReceiver
 {
     public class Program
     {
-        static void Main(string[] args)
+        const string CONNECTION_STRING = "Endpoint=sb://servicebusdemo-kpr.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=yJHI/e+K0DmhoWWtBq4+SjCboYg1q9N8phf/EJOg2Vk=";
+        const string QUEUE_NAME = "personqueue";
+        static ServiceBusClient serviceBusClient = new ServiceBusClient(CONNECTION_STRING);
+
+        async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var receiver = serviceBusClient.CreateReceiver(QUEUE_NAME, new ServiceBusReceiverOptions()
+            {
+                //ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete,
+                ReceiveMode = ServiceBusReceiveMode.PeekLock
+            });
+
+            var messageFromQueue = await receiver.ReceiveMessageAsync();
+
+            var personFromQueue = JsonSerializer.Deserialize<Person>(messageFromQueue.Body);
+
+            Console.WriteLine(personFromQueue.FirstName);
+            Console.WriteLine(personFromQueue.LastName);
         }
     }
 }
